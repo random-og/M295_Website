ef39e76ef9c0ac0b940b075e830d49075af667f7
Brendan Demierre <demierre.brendan@gmail.com>
2022-11-03 00:58:55 +0100

    contact form finished

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,27 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import "./contact.css";
 
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/ff7d53c0-5ad9-11ed-b82c-5d75eaa7ccff";
+
 const Contact = () => {
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+        setTimeout(() => {
+            setSubmitted(true);
+        }, 100);
+    };
+
+    if (submitted) {
+        return (
+            <>
+                <div className="text-2xl">Thank you!</div>
+                <div className="text-md">We'll be in touch soon.</div>
+            </>
+        );
+    }
+
     return (
-        <div className="bw__contact section__margin" id="contact">
+        <div className="bw__contact section__margin">
             <div className="bw__contact-heading">
-                <h1 className="gradient__text">Don't hesitate to contact me</h1>
+                <h1 className="gradient__text">Contact Form</h1>
+                <p>We don't really know where this goes - and I'm not sure we really care. It just happens - whether or not you worried about it or tried to plan it. Paint anything you want on the canvas. Create your own world. Use what you see, don't plan it. Put it in, leave it alone.</p>
             </div>
-            <div className="bw__contact-inputs">
-                <form action="mailto:brendan.demierre@bwdbern.ch" method="POST">
-                <div className="bw__contact-inputs_dropdown">
-                    <label>Sex</label>
-                    <select id="sex" name="sex">
-                        <option value="" disabled selected hidden>Chose your sex</option>
-                        <option value="male">male</option>
-                        <option value="female">female</option>
-                        <option value="divers">divers</option>
-                    </select>
+            <form
+                action={FORM_ENDPOINT}
+                onSubmit={handleSubmit}
+                method="POST"
+                target="_blank"
+                className="bw__contact-form"
+            >
+                <div className="bw__contact-form-input">
+                    <input
+                        type="text"
+                        placeholder="Your name"
+                        name="name"
+                        required
+                    />
+                </div>
+                <div className="bw__contact-form-input">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        required
+                    />
+                </div>
+                <div className="bw__contact-form-input">
+                    <textarea
+                        placeholder="Your message"
+                        name="message"
+                        required
+                    />
+                </div>
+                <div className="bw__contact-form-btn">
+                    <button
+                        type="submit"
+                    >
+                        Send a message
+                    </button>
                 </div>
             </form>
-            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
