9a70a2608f57d689e5f00774112eb908a6d5c91c
Brendan Demierre <demierre.brendan@gmail.com>
2023-08-14 20:04:22 +0200

    Contact diplay fix, mobile login fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { RiMenu3Line, RiCloseLine} from "react-icons/ri";
 import logo from "../../assets/logos/logo_white.svg"
 import test from '../../file/Brendan-Demierre_Files.zip';
 import {RiCloseFill} from "react-icons/ri";
+import { DisplaySettings } from "@mui/icons-material";
 
 
 const Navbar = () => {
@@ -29,8 +30,14 @@ const Navbar = () => {
     const handleSubmit = (event) => {
       //Prevent page reload
       event.preventDefault();
-  
-      var { uname, pass } = document.forms[0];
+      var width = window.innerWidth;
+      
+      if (width > 500) {
+        var { uname, pass } = document.forms[0];
+      }
+      else {
+        var { uname, pass } = document.forms[1];
+      }
   
       // Find user login info
       const userData = database.find((user) => user.username === uname.value);
@@ -121,10 +128,45 @@ const Navbar = () => {
                             <p><a href="#projects">Projects</a></p>
                             <p><a href="#hobbies">Hobbies</a></p>
                             <p><a href="#blog">Contact</a></p>
-                            <div className="bw__navbar-menu_container-links-sign">
-                                <p>Sign in</p>
-                                <button type="button">Sign up</button>
+                          <div className="bw__navbar-menu_container-links-sign">
+                          {toggleLogin
+                              ? <button onClick={() => setToggleLogin(false)} type="button">Login</button>
+                              : <button onClick={() => setToggleLogin(true)} type="button">Login</button>
+                          }
+                          {toggleLogin && (
+                          <div className="bw__navbar-sign_container scale-up-center">
+                            <RiCloseFill className="bw__navbar-sign_container-close" size={27} onClick={() => setToggleLogin(false)}/>
+                              {isSubmitted ? 
+                                <div>
+                                  <a
+                                    href={test}
+                                    download="Brendan-Demierre_Files"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                  >
+                                  <button>Download .zip file</button>
+                                </a>
+                              </div>
+                              : 
+                                <form onSubmit={handleSubmit}>
+                                <div className="input-container">
+                                  <label>Username </label>
+                                  <input type="text" name="uname" required />
+                                  {renderErrorMessage("uname")}
+                                </div>
+                                <div className="input-container">
+                                  <label>Password </label>
+                                  <input type="password" name="pass" required />
+                                  {renderErrorMessage("pass")}
+                                </div>
+                                <div className="button-container">
+                                  <input type="submit" />
+                                </div>
+                              </form>}
                             </div>
+                          )}
+                              
+                          </div>
                         </div>
                     </div>
                 )}
